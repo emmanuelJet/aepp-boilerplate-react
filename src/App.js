dc116d2a54e26cb144ac81e59bcf3ceff6440481
Emmanuel Joseph(JET) <emmanueljet774@gmail.com>
2021-11-26 16:52:09 +0100

    optimized useAeternitySDK hook

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import logo from './logo.svg';
 import useAeternitySDK from './hooks/useAeternitySDK';
 
 const App = () => {
-	useAeternitySDK();
+	const client = useAeternitySDK();
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>See console for Wallet connection status</p>
+        <p>
+					{
+						client
+						? "See console for the connected account details"
+						: "Account not connected"
+					}
+				</p>
         <a
           className="App-link"
           href="https://aeternity.com/awesome-aeternity"
--- src/hooks/useAeternitySDK.js
+++ src/hooks/useAeternitySDK.js
@@ -1,36 +1,33 @@
-import {
-	useState,
-	useEffect
-} from 'react';
+import { useRef, useState, useEffect } from 'react';
 import { AmountFormatter } from '@aeternity/aepp-sdk';
 
 import { aeternitySDK } from "../utils/aeternity";
 
 const useAeternitySDK = () => {
+	let sdk = useRef();
 	const [client, clientReady] = useState(null);
-	console.log("Client Status", client);
 
 	useEffect(() => {
 		(async () => {
-			let sdk = await aeternitySDK();
+			try {
+				sdk.current = await aeternitySDK();
 
-			if (!sdk) {
-				console.error("Error fetching aeternitySDK");
+				let address = await sdk.current.address();
+				let balance = await sdk.current.balance(address, {
+					format: AmountFormatter.AE_AMOUNT_FORMATS.AE
+				});
 
+				console.log("Current Address:", address);
+				console.log("Current Balance:", balance + AmountFormatter.AE_AMOUNT_FORMATS.AE);
+				clientReady(sdk.current);
+			} catch (err) {
+				console.error("Error fetching aeternitySDK", err);
 				clientReady(false)
 			}
-
-			let address = await sdk.address();
-			let balance = await sdk.balance(address, {
-				format: AmountFormatter.AE_AMOUNT_FORMATS.AE
-			});
-
-			console.log("Current Address:", address);
-			console.log("Current Balance:", balance + AmountFormatter.AE_AMOUNT_FORMATS.AE);
-
-			clientReady(true);
 		})();
-  }, []);
+  }, [sdk]);
+
+	return client;
 }
 
 export default useAeternitySDK;
